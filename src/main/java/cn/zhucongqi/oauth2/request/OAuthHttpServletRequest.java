5895df2aa6a2ae7beebf3d25edf0f2d79851e608
BruceZCQ <brucezcq@gmail.com>
2018-07-08 22:59:20 +0800

    add OAuthHttpServletRequest wrapper the HttpServletRequest

---
+++ src/main/java/cn/zhucongqi/oauth2/request/OAuthHttpServletRequest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package cn.zhucongqi.oauth2.request;
+
+import java.io.Serializable;
+
+/**
+ * OAuthHttpServletRequest.java
+ * @author Jobsz
+ *
+ */
+public class OAuthHttpServletRequest implements Serializable {
+
+	private static final long serialVersionUID = -4432512843521321200L;
+
+}
